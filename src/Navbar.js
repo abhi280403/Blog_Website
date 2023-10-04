0fe5983ea4b265e6ae0034a39cd5f27fb84aa943
abhi280403 <abhijith1024283@gmail.com>
2023-10-04 19:19:09 +0530

    First commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const navbar = () => {
+    return ( 
+     <div className = "navbar">
+        <h1>The Abhijith Blog</h1>
+        <div className ="links">
+            <a href ="/">Home</a>
+            <a href = "/create">New Blog</a>
+        </div>
+     </div>
+     );
+}
+ 
+export default navbar;
